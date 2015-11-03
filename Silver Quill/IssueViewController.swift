a9c4cecca098b81a8e0ad9ed6ce367b771385663
qscgy <sam.ehrenstein@gmail.com>
2015-11-02 21:28:11 -0500

    first commit

---
+++ Silver Quill/IssueViewController.swift
@@ -0,0 +1,37 @@
+//
+//  IssueViewController.swift
+//  Silver Quill
+//
+//  Created by Sam Ehrenstein on 11/2/15.
+//  Copyright © 2015 MBHS Smartphone Programming Club. All rights reserved.
+//
+
+import UIKit
+
+class IssueViewController: UIViewController {
+
+    @IBOutlet var pdfViewer: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let path=NSBundle.mainBundle().pathForResource("Linguistics34", ofType: "pdf")
+        pdfViewer.loadRequest(NSURLRequest(URL: NSURL(fileURLWithPath: path!)))
+        //print("done")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
